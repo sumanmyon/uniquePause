fc8502ecbaca41874acf29979e26183afc80080c
Suman Neupane <sumanmyon.sam@gmail.com>
2017-08-23 21:31:08 +0545

    menu bar demo

---
+++ src/Controller/MenuBarD.java
@@ -0,0 +1,31 @@
+package Controller;
+
+import javax.swing.JFrame;
+
+public class MenuBarD extends JFrame {
+	
+	public MenuBarD(){
+		initUI();
+	}
+
+	private void initUI() {
+		// TODO Auto-generated method stub
+		createMenuBar();
+		
+		setTitle("MenuBarD");
+		setSize(360,250);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	private void createMenuBar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
